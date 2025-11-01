c79ed0ce7e4a27ac0d3422ec45e9bf9027c7b4d6
Noah <104743939+buysnoah@users.noreply.github.com>
2025-11-01 20:48:54 +0200

    Add display.js and styles.css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const { Todo } = require('./todos.js');
+const display = require('./display.js');
 
 const todo = new Todo("Make this app work", "Make it follow the specification. Work work work", "2025-11-01", 5, "Todo App");
 
