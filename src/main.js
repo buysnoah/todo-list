5daba5955f5fe6812a6ea9945bd368e051b5ce6d
Noah <104743939+buysnoah@users.noreply.github.com>
2025-11-01 23:11:12 +0200

    Add creation of dummy todos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const { Todo } = require('./todos.js');
 const display = require('./display.js');
 
-const todo = new Todo("Make this app work", "Make it follow the specification. Work work work", "2025-11-01", 5, "Todo App");
+for (let i = 0; i <= 5; i++) {
+    const todo = new Todo("Make this app work", "Make it follow the specification. Work work work", "2025-11-01", i, "Todo App");
 
-console.log(todo);
+    const todoElement = display.createTodoElement(todo);
+    display.appendElement("body", todoElement);
+}
