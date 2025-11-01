ceb4b8cd6091f31e49a93ce6666c76428df61a24
Noah <104743939+buysnoah@users.noreply.github.com>
2025-11-01 23:12:16 +0200

    Refactor everything and add material icons

--- src/display.js
+++ src/display.js
@@ -1,1 +1,71 @@
 require("./styles.css");
+
+function appendElement(parentQuery, child) {
+    document.querySelector(parentQuery).appendChild(child)
+}
+
+function createElement(type, className) {
+    const el = document.createElement(type);
+    el.classList.add(className);
+
+    return el;
+}
+
+
+
+
+function createTodoElement(todo) {
+    const todoDiv = createElement("div", "todo");
+    todoDiv.dataset.priority = todo.priority;
+    todoDiv.dataset.done = todo.done;
+
+    const doneCheckbox = createElement("input", "done");
+    doneCheckbox.type = "checkbox";
+    doneCheckbox.checked = todo.done;
+    doneCheckbox.addEventListener("change", e => {
+        todoDiv.dataset.done = doneCheckbox.checked;
+    });
+
+    const infoDiv = createElement("div", "info");
+
+    const title = createElement("h1", "title");
+    title.textContent = todo.title;
+
+    const description = createElement("p", "description");
+    description.textContent = todo.description;
+
+    const date = createElement("span", "date");
+    date.textContent = todo.formatDueDate("y-M-d");
+
+    let toggle = description.style.display === "none";
+    const expandButton = createElement("button", "expand");
+    expandButton.classList.add("material-symbols-outlined");
+
+    function updateExpand() {
+        if (toggle) {
+            description.style.display = "none";
+            expandButton.textContent = "arrow_drop_down";
+        } else {
+            description.style.display = "block";
+            expandButton.textContent = "arrow_drop_up";
+        };
+    }
+    updateExpand();
+
+    expandButton.addEventListener("click", (e) => {
+        toggle = !toggle;
+        updateExpand();
+    });
+
+    infoDiv.appendChild(title);
+    infoDiv.appendChild(description);
+    infoDiv.appendChild(date);
+
+    todoDiv.appendChild(doneCheckbox);
+    todoDiv.appendChild(infoDiv);
+    todoDiv.appendChild(expandButton);
+
+    return todoDiv;
+}
+
+module.exports = { appendElement, createElement, createTodoElement }
